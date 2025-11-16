0db7c0fd3dc504d47833f121a2dc48ba368fd22e
admin <machinetowerbnb@gmail.com>
2025-11-16 12:52:58 +0530

    Updated email confirm screen

--- src/app/features/email-confirmation/email-confirmation.ts
+++ src/app/features/email-confirmation/email-confirmation.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
-import { CommonModule } from '@angular/common';
 import { TopNav } from '../top-nav/top-nav';
-
 import { TranslatePipe } from '../../pipes/translate-pipe';
-import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-email-confirmation',
@@ -13,6 +12,55 @@ import { RouterModule } from '@angular/router';
   templateUrl: './email-confirmation.html',
   styleUrl: './email-confirmation.scss'
 })
-export class EmailConfirmation {
+export class EmailConfirmation implements OnInit {
 
+  email: string | null = null;
+  otp: string | null = null;  // 'code' from query params
+
+  constructor(
+    private route: ActivatedRoute,
+    private auth: AuthService,
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) { }
+
+  ngOnInit(): void {
+    // Read query params: /email-confirm?code=1234&email=mani@gmail.com
+    this.route.queryParams.subscribe(params => {
+      this.otp = params['code'] || null;
+      this.email = params['email'] || null;
+
+      if (this.email && this.otp) {
+        this.verifyOtp();
+        console.log(this.email,this.otp);
+      } else {
+        console.error("Missing query params.");
+      }
+    });
+  }
+
+  verifyOtp() {
+    const payload = {
+      email: this.email,
+      otp: this.otp
+    };
+
+    // this.auth.verifyOtp(payload).subscribe({
+    //   next: (res) => {
+    //     if (res.success === 200) {
+
+    //       // Save userId in localStorage (browser only)
+    //       if (isPlatformBrowser(this.platformId)) {
+    //         localStorage.setItem("userId", res.data.userId);
+    //       }
+
+    //       // Redirect to home
+    //       this.router.navigate(['/home']);
+    //     }
+    //   },
+    //   error: (err) => {
+    //     console.error("Email verification failed", err);
+    //   }
+    // });
+  }
 }
