b5cc964f64126036051f1ad0c878f87ea70a9968
gembalih@hdfclife.com <gembalih@hdfclife.com>
2025-11-26 19:42:25 +0530

    Ui revamp

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://tower-backend-o9k1.onrender.com/api' // your backend base URL
+  // apiUrl: 'https://tower-backend-o9k1.onrender.com/api' // your backend base URL
+  apiUrl: 'http://localhost:8080/api' // your backend base URL
 };
