1cf192f3f283a3538b5915bae749974afd46c294
machinetowerbnb-prod <machinetowerbnb@gmail.com>
2025-11-26 22:04:48 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // apiUrl: 'https://tower-backend-o9k1.onrender.com/api' // your backend base URL
-  apiUrl: 'http://localhost:8080/api' // your backend base URL
+  apiUrl: 'https://api.machine-towerbnb.com' // your backend base URL
 };
