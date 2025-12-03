924e5bf226aa9f149ae8ddb54bd2d6d166606bc8
admin <machinetowerbnb@gmail.com>
2025-12-03 21:09:47 +0530

    UI Fixes

--- src/app/features/verify-email/verify-email.ts
+++ src/app/features/verify-email/verify-email.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { TopNav } from '../top-nav/top-nav';
+import { Router } from '@angular/router';
 
 import { TranslatePipe } from '../../pipes/translate-pipe';
 import { RouterModule } from '@angular/router';
@@ -13,6 +14,14 @@ import { RouterModule } from '@angular/router';
   templateUrl: './verify-email.html',
   styleUrl: './verify-email.scss'
 })
-export class VerifyEmail {
+export class VerifyEmail implements OnInit {
+
+  constructor(
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    setTimeout(() => this.router.navigate(['/signin']), 50000);
+  }
 
 }
